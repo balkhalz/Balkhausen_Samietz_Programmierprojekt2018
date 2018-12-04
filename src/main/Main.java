2aeddcb349c73e997e9b82a95e89ef3d316a344f
Jannick Samietz <Jannick Samietz@141.58.58.238>
2018-12-04 13:17:13 +0100

    Working Reading Algorithm ~75s, removed deprecated code.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,27 +2,11 @@ package main;
 
 import data.Data;
 
-class RunReadData extends Thread {
-	public void run(String data) {
-		if (data.equals("readData")) {
-			Data.readData();
-		} else if (data.equals("writeData")) {
-			Data.writeData();
-		} else {
-			System.out.println("Wrong Input.");
-		}
-	}
-}
+public class Main {
 
-public class Main extends Thread {
 	public static void main(String[] args) {
-		RunReadData readData = new RunReadData();
-		RunReadData writeData = new RunReadData();
 		Data.initialize();
-		readData.run("readData");
-		writeData.run("writeData");
-		// Data.readData();
-		// Data.writeData();
-		Data.testData();
+		Data.readAndWrite();
 	}
+
 }
