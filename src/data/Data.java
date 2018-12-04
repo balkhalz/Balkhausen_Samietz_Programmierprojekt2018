2d976acc485b5198641e96eac4e28dd373af7264
Jannick Samietz <Jannick Samietz@141.58.58.238>
2018-12-04 13:17:13 +0100

    Implemented a working reading algorithm ~75s. Implemented a simple stopwatch.

--- src/data/Data.java
+++ src/data/Data.java
@@ -7,115 +7,134 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import utility.Utility;
+
 public class Data {
 
-	private static boolean readNodes, readEdges;
-	private static Scanner scanner;
 	private static int nodeAmount, edgeAmount;
+	private static File sourceFile;
+	private static Scanner scanner;
+	
+	private ArrayList<String[]> tempNodeData, tempEdgeData;
 
-	private static int[] edgeOffset;
-	private static ArrayList<String[]> nodeDataList, edgeDataList;
-	private static String[][] nodeArray;
-	private static int[][] edgeArray;
+	public static int[] nodeOffset; // length of nodeAmount
+	public static double[] xLatitude, yLatitude; // length of nodeAmount, sorted by unique nodeID by array index
+	public static int[] startNodeID, endNodeID, edgeValue; // length of edgeAmount, sorted by unique edgeID as array
+															// index
 
-	public static void initialize() {
-		try {
-			scanner = new Scanner(new BufferedReader(new FileReader(readFilePath().getAbsolutePath())));
-			scannerSkipLines(4, scanner);
-			nodeAmount = scanner.nextInt();
-			edgeAmount = scanner.nextInt();
-			scanner.nextLine();
-			// System.out.println(nodeAmount + "\n" + edgeAmount);
-			edgeOffset = new int[nodeAmount];
-			nodeArray = new String[nodeAmount][3];
-			edgeArray = new int[edgeAmount][3];
-
-			nodeDataList = new ArrayList<String[]>(nodeAmount);
-			edgeDataList = new ArrayList<String[]>(edgeAmount);
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static File readFilePath() {
-		return new File("src/stgtregbz.fmi");
-	}
-
-	public static void readData() {
-		readNodes = false;
-		for (int ctr = 0; ctr < nodeAmount; ctr++) {
-			nodeDataList.add(ctr, scanner.nextLine().split(" "));
-		}
-		readNodes = true;
-		System.out.println("reading nodes finished");
-
-		readEdges = false;
-		for (int ctr = 0; ctr < edgeAmount; ctr++) {
-			edgeDataList.add(ctr, scanner.nextLine().split(" "));
-		}
-		readEdges = true;
-		System.out.println("reading edges finished");
-
+	/**
+	 * This method returns the filepath of the sourcefile.
+	 * 
+	 * @return
+	 */
+	private static File readFilePath() {
+		sourceFile = new File("src/stgtregbz.fmi");
+		return sourceFile;
 	}
 
 	/**
-	 * This method is used to write the data from the created List to usable Arrays.
-	 * It is designed to function multithreaded, so it never skips ahead of the
-	 * fileReader.
+	 * This method skips the next line for a given amount of times for a given
+	 * scanner.
+	 * 
+	 * @param skips
+	 * @param scanner
 	 */
-	public static void writeData() {
-		int nodeCounter = 0;
-		edgeOffset[0] = 0;
-		while (readNodes = false) {
-		}
-		for (int ctr = 0; ctr < nodeAmount; ctr++) {
-			nodeArray[ctr][0] = nodeDataList.get(ctr)[2];
-			nodeArray[ctr][1] = nodeDataList.get(ctr)[3];
-		}
-
-		while (readEdges = false) {
-
-		}
-		for (int ctr = 0; ctr < edgeAmount; ctr++) {
-			edgeArray[ctr][0] = Integer.parseInt(edgeDataList.get(ctr)[0]);
-			edgeArray[ctr][1] = Integer.parseInt(edgeDataList.get(ctr)[1]);
-			edgeArray[ctr][2] = Integer.parseInt(edgeDataList.get(ctr)[2]);
-			if (Integer.parseInt(edgeDataList.get(ctr)[0]) != nodeCounter) {
-				nodeCounter++;
-				edgeOffset[nodeCounter] = ctr;
-			}
-		}
-		System.out.println("writing arrays finished");
-	}
-
 	private static void scannerSkipLines(int skips, Scanner scanner) {
 		for (int ctr = 0; ctr < skips; ctr++) {
 			scanner.nextLine();
 		}
 	}
 
-	public static int[] getEdgeOffset() {
-		return edgeOffset;
+	public static void initialize() {
+		// initializing the scanner
+		try {
+			scanner = new Scanner(new BufferedReader(new FileReader(readFilePath().getAbsolutePath())));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		// utilizing the scanner, the amount of nodes + edges are read
+		scannerSkipLines(4, scanner);
+		nodeAmount = scanner.nextInt();
+		edgeAmount = scanner.nextInt();
+		scanner.nextLine();
+
+		// initializing the arrays to be used with the amount of nodes and edges
+		nodeOffset = new int[nodeAmount];
+		xLatitude = new double[nodeAmount];
+		yLatitude = new double[nodeAmount];
+
+		startNodeID = new int[edgeAmount];
+		endNodeID = new int[edgeAmount];
+		edgeValue = new int[edgeAmount];
+
+		System.out.println("Initialized and ready to rumble!");
+		// System.out.println(nodeAmount + " " + edgeAmount);
 	}
 
-	public static int[][] getEdgeArray() {
-		return edgeArray;
+	public static void readAndWrite() {
+		Utility.startTimer();
+		readNodes();
+		readEdges();
+		createOffset();
+		System.out.println("Finished in: " + Utility.endTimer() + " seconds!");
 	}
 
-	public static void testData() {
-		if (nodeArray[0][0].equals("48.66743380000000485") && nodeArray[0][1].equals("9.24459110000000095")
-				&& nodeArray[1132112][0].equals("48.64209470000000124")
-				&& nodeArray[1132112][1].equals("9.01481120000000047")) {
-			System.out.println("nodeArray setup correctly!");
+	/**
+	 * This method extrapolates all node data needed to run the dijkstra algorithm.
+	 */
+	private static void readNodes() {
+		String[] tempArray = null;
+		for (int ctr = 0; ctr < nodeAmount; ctr++) {
+			tempArray = scanner.nextLine().split(" ");
+			xLatitude[ctr] = Double.parseDouble(tempArray[2]);
+			yLatitude[ctr] = Double.parseDouble(tempArray[3]);
 		}
-		if (edgeArray[0][0] == 0 && edgeArray[0][1] == 1104366 && edgeArray[0][2] == 55
-				&& edgeArray[2292886][0] == 1132112 && edgeArray[2292886][1] == 1131157
-				&& edgeArray[2292886][2] == 462) {
-			System.out.println("edgeArray setup correctly!");
+		// System.out.println("Reading and writing nodes finished");
+	}
+
+	/**
+	 * This method extrapolates all edge data needed to run the dijkstra algorithm.
+	 */
+	private static void readEdges() {
+		String[] tempArray = null;
+		for (int ctr = 0; ctr < edgeAmount; ctr++) {
+			tempArray = scanner.nextLine().split(" ");
+			startNodeID[ctr] = Integer.parseInt(tempArray[0]);
+			endNodeID[ctr] = Integer.parseInt(tempArray[1]);
+			edgeValue[ctr] = Integer.parseInt(tempArray[2]);
 		}
-		if (edgeOffset[0] == 0 && edgeOffset[1132112] == 2292885) {
-			System.out.println("edgeOffsetArray setup correctly!");
+		// System.out.println("Reading and writing edges finished");
+	}
+
+	/**
+	 * This method creates the offset-table used for faster access to the
+	 * startNodeIDs.
+	 */
+	private static void createOffset() {
+		int index = -1;
+		for (int i = 0; i < edgeAmount; i++) {
+			if (startNodeID[i] != index) {
+				index = startNodeID[i];
+				nodeOffset[index] = i;
+			}
 		}
+		// System.out.println("Offset created!");
+	}
+
+	public static int[] getOffsetArray() {
+		return nodeOffset;
+	}
+
+	public static int[] getStartNodeIDArray() {
+		return startNodeID;
+	}
+
+	public static int[] getEndNodeIDArray() {
+		return endNodeID;
+	}
+
+	public static int[] getedgeValueArray() {
+		return edgeValue;
 	}
 }
