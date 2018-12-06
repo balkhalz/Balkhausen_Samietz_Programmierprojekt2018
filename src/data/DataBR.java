144cb7334dd6cc0afab6cfd307c44824e8840e74
paul <paul@DESKTOP-C6Q2QB6.local>
2018-12-06 18:14:08 +0100

    DataBR - Reads Nodes with bufferedReader

---
+++ src/data/DataBR.java
@@ -0,0 +1,148 @@
+package data;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import utility.Utility;
+
+public class DataBR {
+
+	private static int nodeAmount, edgeAmount;
+	private static File sourceFile;
+	private static BufferedReader reader;
+
+	public static int[] nodeOffset; // length of nodeAmount
+	public static double[] latitudes, longitudes; // length of nodeAmount, sorted by unique nodeID by array index
+	
+	public static int[] startNodeID, endNodeID, edgeValue; // length of edgeAmount, sorted by unique edgeID as array
+															// index
+
+	/**
+	 * This method returns the filepath of the sourcefile.
+	 * 
+	 * @return
+	 */
+	private static File readFilePath() {
+		sourceFile = new File("src/germany.fmi");
+		return sourceFile;
+	}
+
+	/**
+	 * This method skips the next line for a given amount of times for a given
+	 * scanner.
+	 * 
+	 * @param skips
+	 * @param scanner
+	 */
+	private static void skipLines(int skips) {
+		try {
+			for (int ctr = 0; ctr < skips; ctr++) {
+				reader.readLine();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void initialize() {
+		// initializing the scanner
+		try {
+			reader = new BufferedReader(new FileReader(readFilePath()));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		skipLines(5);
+		try {
+			nodeAmount = Integer.parseInt(reader.readLine());
+			edgeAmount = Integer.parseInt(reader.readLine());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// initializing the arrays to be used with the amount of nodes and edges
+		nodeOffset = new int[nodeAmount];
+		latitudes = new double[nodeAmount];
+		longitudes = new double[nodeAmount];
+
+		startNodeID = new int[edgeAmount];
+		endNodeID = new int[edgeAmount];
+		edgeValue = new int[edgeAmount];
+
+		System.out.println("Initialized and ready to rumble!");
+		// System.out.println(nodeAmount + " " + edgeAmount);
+	}
+
+	public static void readAndWrite() {
+		Utility.startTimer();
+		readNodes();
+		readEdges();
+		System.out.println("Finished in: " + Utility.endTimer() + " seconds!");
+	}
+
+	/**
+	 * This method extrapolates all node data needed to run the dijkstra algorithm.
+	 */
+	private static void readNodes() {
+		String[] tempArray = null;
+		for (int ctr = 0; ctr < nodeAmount; ctr++) {
+			
+			try {
+				tempArray = reader.readLine().split(" ");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			latitudes[ctr] = Double.parseDouble(tempArray[2]);
+			longitudes[ctr] = Double.parseDouble(tempArray[3]);
+		}
+		// System.out.println("Reading and writing nodes finished");
+	}
+
+	/**
+	 * This method extrapolates all edge data needed to run the dijkstra algorithm.
+	 */
+	private static void readEdges() {
+		String[] tempArray = null;
+		int thisNode = 0;
+		int lastNode = -1;
+		for (int ctr = 0; ctr < edgeAmount; ctr++) {
+
+			try {
+				tempArray = reader.readLine().split(" ");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			thisNode = Integer.parseInt(tempArray[0]);
+
+			startNodeID[ctr] = thisNode;
+			endNodeID[ctr] = Integer.parseInt(tempArray[1]);
+			edgeValue[ctr] = Integer.parseInt(tempArray[2]);
+
+			if (thisNode != lastNode)
+				nodeOffset[thisNode] = ctr;
+
+			lastNode = thisNode;
+		}
+		// System.out.println("Reading and writing edges finished");
+	}
+
+	public static int[] getOffsetArray() {
+		return nodeOffset;
+	}
+
+	public static int[] getStartNodeIDArray() {
+		return startNodeID;
+	}
+
+	public static int[] getEndNodeIDArray() {
+		return endNodeID;
+	}
+
+	public static int[] getedgeValueArray() {
+		return edgeValue;
+	}
+
+}
