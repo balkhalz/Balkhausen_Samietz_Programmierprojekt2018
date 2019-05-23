44136ddd4cde83973eb5c8f2247c5362708d9e9a
paul <paul@DESKTOP-C6Q2QB6.local>
2019-05-23 05:29:14 +0200

    Dijkstra Bugfix

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,15 +1,15 @@
 public class Dijkstra {
-
+	
 	private static int pivotSource;
-
-	private static int[] edgeOffset, endNodeID, edgeValue;
-
+	
+	private static int[] edgeOffset, startNodeID, endNodeID, edgeValue;
+	
 	private static int amountOfEdges, amountOfNodes;
-
-	private static boolean[] nodeReachable, minimalDistanceFound;
-	private static int priorityQueueLength; // repraesentiert die Laenge der priorityQueue
-	private static int[] priorityQueue; // enthaelt Verweise auf distanceArray
-	private static int[] distanceArray; // enthaelt Abstaende von sourceNode zu entsprechender Node
+	
+	private static boolean[] inPriorityQueue, minimalDistanceFound;
+	private static int priorityQueueLength;	//	repräsentiert die Lände der priorityQueue
+	private static int[] priorityQueue;	//	enthält Verweise auf distanceArray
+	private static int[] distanceArray;	//	enthält Abstände von sourceNode zu entsprechender Node
 
 	/**
 	 * calculates the distance from the source to the target
@@ -19,88 +19,69 @@ public class Dijkstra {
 	 * @return distance from source to target
 	 */
 	public static int setSourceAndTarget(int source, int target) {
-
-		Utility.startTimer();
-
+		
 		edgeOffset = Data.getOffsetArray();
+		startNodeID = Data.getStartNodeIDArray();
 		endNodeID = Data.getEndNodeIDArray();
 		edgeValue = Data.getEdgeValueArray();
-
+		
 		amountOfNodes = edgeOffset.length;
 		amountOfEdges = endNodeID.length;
-
+		
 		priorityQueueLength = 1;
 
-		nodeReachable = new boolean[amountOfNodes];
+		inPriorityQueue = new boolean[amountOfNodes];
 		minimalDistanceFound = new boolean[amountOfNodes];
 		priorityQueue = new int[amountOfNodes];
-		distanceArray = new int[amountOfEdges];
-
+		distanceArray = new int[amountOfNodes];
+		
 		for (int ctr = 0; ctr < distanceArray.length; ctr++)
 			distanceArray[ctr] = Integer.MAX_VALUE;
-
+		
 		priorityQueue[0] = source;
 		distanceArray[source] = 0;
+		
 
 		while (true) {
-
+			
 			int closestNode = priorityQueue[0];
 			int tempDist = distanceArray[closestNode];
-			int closestNodeTarget;
-
+			int closestNodeTarget = -1;
+			int offset = edgeOffset[closestNode];
+			
 			minimalDistanceFound[closestNode] = true;
-
-			// Aus Node zeigende Edges werden mit edgeOffset[Node] und edgeOffset[Node + 1]
-			// ermittelt
-			int b = edgeOffset[closestNode];
-			int n;
-
-			// falls b = -1 ist, wurde tempNode eine sackgasse und hat keine Edges, die aus
-			// ihr zeigen
-			if (b != -1) {
-
-				// da edgeOffset[tempNode + 1] eine Exception wefen kann, wird hier nachgefragt
-				if (closestNode + 1 == amountOfNodes)
-					n = amountOfEdges;
-				else
-					n = edgeOffset[closestNode + 1];
-
-				for (int ctr = b; ctr < n; ctr++) {
-					closestNodeTarget = endNodeID[ctr];
-
+			
+			if (offset != -1)
+				while (offset != amountOfEdges && closestNode == startNodeID[offset]) {
+					closestNodeTarget = endNodeID[offset];
+					
 					// verhindert Schleifen
 					if (!minimalDistanceFound[closestNodeTarget])
-						if (distanceArray[closestNodeTarget] > tempDist + edgeValue[ctr]) {
-							distanceArray[closestNodeTarget] = tempDist + edgeValue[ctr];
-
+						if (distanceArray[closestNodeTarget] > tempDist + edgeValue[offset]) {
+							distanceArray[closestNodeTarget] = tempDist + edgeValue[offset];
+							
 							// falls noch nicht in priorityQueue, Aufnahme in diese
-							if (!nodeReachable[closestNodeTarget]) {
+							if (!inPriorityQueue[closestNodeTarget]) {
 								priorityQueue[priorityQueueLength] = closestNodeTarget;
 								priorityQueueLength++;
-								nodeReachable[closestNodeTarget] = true;
+								inPriorityQueue[closestNodeTarget] = true;
 							}
 						}
+					offset++;
 				}
-			}
-
-			// eine Node wurde abgehandelt, deswegen wird priorityQueue[0] entfernt
+			
+			if (priorityQueueLength == 0)
+				return -1;
+			
 			priorityQueueLength--;
 			priorityQueue[0] = priorityQueue[priorityQueueLength];
-			nodeReachable[closestNode] = false;
-
-			// die Node mit der kuerzesten Distanz, die noch nicht abgehandelt wurde wird an
-			// prioQueue[0] gesetzt
+			inPriorityQueue[closestNode] = false;
+			
 			minHeapSort();
-
-			/*
-			 * falls keine kuerzerer Distanz mehr moeglich ist, d.h. distanz[target] <
-			 * distanz[naechster Node], wird die Distanz zurueckgegeben
-			 */
-			if (distanceArray[target] < distanceArray[priorityQueue[0]]) {
+			
+			if (distanceArray[target] < distanceArray[priorityQueue[0]])
 				return distanceArray[target];
-			}
 		}
-
 	}
 
 	/**
@@ -111,7 +92,7 @@ public class Dijkstra {
 	public static void setPivotSource(int sourceToSet) {
 		pivotSource = sourceToSet;
 	}
-
+	
 	/**
 	 * calculates the distance from the pivot source to the given target
 	 * 
@@ -122,6 +103,7 @@ public class Dijkstra {
 		return setSourceAndTarget(pivotSource, target);
 	}
 
+	
 	/**
 	 * calculates the distance from the pivot source to multiple targets
 	 * 
@@ -129,127 +111,126 @@ public class Dijkstra {
 	 * @return distance between pivotSource to multiple targets
 	 */
 	public static int[] fromPivotSourceToTargets(int[] targets) {
-
+		
 		boolean finished = false;
-
+		
 		edgeOffset = Data.getOffsetArray();
+		startNodeID = Data.getStartNodeIDArray();
 		endNodeID = Data.getEndNodeIDArray();
 		edgeValue = Data.getEdgeValueArray();
-
+		
 		amountOfNodes = edgeOffset.length;
 		amountOfEdges = endNodeID.length;
-
+		
 		priorityQueueLength = 1;
 
-		nodeReachable = new boolean[amountOfNodes];
+		inPriorityQueue = new boolean[amountOfNodes];
 		minimalDistanceFound = new boolean[amountOfNodes];
 		priorityQueue = new int[amountOfNodes];
 		distanceArray = new int[amountOfEdges];
-
+		
 		for (int ctr = 0; ctr < distanceArray.length; ctr++)
 			distanceArray[ctr] = Integer.MAX_VALUE;
-
+		
 		priorityQueue[0] = pivotSource;
 		distanceArray[pivotSource] = 0;
-
+		
+		
 		while (true) {
-
+			
 			int closestNode = priorityQueue[0];
 			int tempDist = distanceArray[closestNode];
-			int closestNodeTarget;
-
+			int closestNodeTarget = -1;
+			int offset = edgeOffset[closestNode];
+			
 			minimalDistanceFound[closestNode] = true;
-
-			// Aus Node zeigende Edges werden mit edgeOffset[Node] und edgeOffset[Node + 1]
-			// ermittelt
-			int b = edgeOffset[closestNode];
-			int n;
-
-			// falls b = -1 ist, wurde tempNode eine sackgasse und hat keine Edges, die aus
-			// ihr zeigen
-			if (b != -1) {
-
-				// da edgeOffset[tempNode + 1] eine Exception werfen kann, wird hier nachgefragt
-				if (closestNode + 1 == amountOfNodes)
-					n = amountOfEdges;
-				else
-					n = edgeOffset[closestNode + 1];
-
-				for (int ctr = b; ctr < n; ctr++) {
-					closestNodeTarget = endNodeID[ctr];
-
+			
+			if (offset != -1)
+				while (offset != amountOfEdges && closestNode == startNodeID[offset]) {
+					closestNodeTarget = endNodeID[offset];
+					
 					// verhindert Schleifen
 					if (!minimalDistanceFound[closestNodeTarget])
-						if (distanceArray[closestNodeTarget] > tempDist + edgeValue[ctr]) {
-							distanceArray[closestNodeTarget] = tempDist + edgeValue[ctr];
-
+						if (distanceArray[closestNodeTarget] > tempDist + edgeValue[offset]) {
+							distanceArray[closestNodeTarget] = tempDist + edgeValue[offset];
+							
 							// falls noch nicht in priorityQueue, Aufnahme in diese
-							if (!nodeReachable[closestNodeTarget]) {
+							if (!inPriorityQueue[closestNodeTarget]) {
 								priorityQueue[priorityQueueLength] = closestNodeTarget;
 								priorityQueueLength++;
-								nodeReachable[closestNodeTarget] = true;
+								inPriorityQueue[closestNodeTarget] = true;
 							}
 						}
+					offset++;
 				}
-			}
-			priorityQueueLength--;
-			priorityQueue[0] = priorityQueue[priorityQueueLength];
-			nodeReachable[closestNode] = false;
-
-			minHeapSort();
-
-			finished = true;
-
-			// falls fuer nur 1 target kein ergebnis gefunden wurde, ist die Suche NICHT
-			// beendet
-			for (int target : targets) {
-				if (!minimalDistanceFound[target]) {
-					finished = false;
+			
+			if (priorityQueueLength == 0) {				// nicht verbunden
+				for (int ctr = 0; ctr < targets.length; ctr++)
+					if (distanceArray[targets[ctr]] == Integer.MAX_VALUE) {
+						distanceArray[targets[ctr]] = -1;
+						minimalDistanceFound[targets[ctr]] = true;
+					}
+			} else {
+				priorityQueueLength--;
+				priorityQueue[0] = priorityQueue[priorityQueueLength];
+				inPriorityQueue[closestNode] = false;
+				
+				minHeapSort();
+				
+				finished = true;
+				
+				// falls für nur 1 target kein ergebniss gefunden wurde, ist die Suche NICHT beendet
+				for (int target : targets) {
+					if (!minimalDistanceFound[target]) {
+						finished = false;
+						break;
+					}
 				}
 			}
 			if (finished) {
-				for (int ctr = 0; ctr < targets.length; ctr++) {
+				for (int ctr = 0; ctr < targets.length; ctr++)
 					targets[ctr] = distanceArray[targets[ctr]];
-					System.out.println(targets[ctr]);
-				}
 				return targets;
 			}
 		}
 	}
-
+		
 	/**
 	 * minHeap sortierer
 	 * 
-	 * sortiert die ersten n = nodesToVisit nodes der PrioQueue anhand der Laenge x
-	 * = distanceArray[prioQueue[i]] von der Node i
+	 * sortiert die ersten n = nodesToVisit nodes der PrioQueue anhand der Länge
+	 * x = distanceArray[prioQueue[i]] von der Node i
 	 * 
 	 */
 	private static void minHeapSort() {
-
+		
 		if (priorityQueueLength == 0)
 			return;
-
+		
 		int firstParentNode = (priorityQueueLength - 2) / 2;
 		int childNode = firstParentNode * 2 + 2;
-
+		
 		int parentPointer = priorityQueue[firstParentNode];
 
-		// bei erstem durchlauf ArrayOutOfBounds moeglich, deswegen seperater durchlauf
-		// mit extra if-Abfrage
-		if (childNode < priorityQueueLength && distanceArray[parentPointer] > distanceArray[priorityQueue[childNode]]) {
+		// bei erstem durchlauf ArrayOutOfBounds möglich, deswegen seperater durchlauf mit extra if-Abfrage
+		if (childNode < priorityQueueLength &&
+				distanceArray[parentPointer] > distanceArray[priorityQueue[childNode]]) {
 			priorityQueue[firstParentNode] = priorityQueue[childNode];
 			priorityQueue[childNode] = parentPointer;
-
+			
 			parentPointer = priorityQueue[firstParentNode];
 		}
-
+		
 		childNode--;
 
-		if (childNode < priorityQueueLength && distanceArray[parentPointer] > distanceArray[priorityQueue[childNode]]) {
+		if (childNode < priorityQueueLength &&
+				distanceArray[parentPointer] > distanceArray[priorityQueue[childNode]]) {
 			priorityQueue[firstParentNode] = priorityQueue[childNode];
 			priorityQueue[childNode] = parentPointer;
 		}
-
+		
+		
+		
 		for (int parentNode = firstParentNode - 1; parentNode != -1; parentNode--) {
 			childNode--;
 			parentPointer = priorityQueue[parentNode];
@@ -257,10 +238,10 @@ public class Dijkstra {
 			if (distanceArray[parentPointer] > distanceArray[priorityQueue[childNode]]) {
 				priorityQueue[parentNode] = priorityQueue[childNode];
 				priorityQueue[childNode] = parentPointer;
-
+				
 				parentPointer = priorityQueue[parentNode];
 			}
-
+			
 			childNode--;
 
 			if (distanceArray[parentPointer] > distanceArray[priorityQueue[childNode]]) {
@@ -269,4 +250,60 @@ public class Dijkstra {
 			}
 		}
 	}
+	
+	public static void nodesConnectedTo(int source) {
+		Utility.startTimer();
+		
+		edgeOffset = Data.getOffsetArray();
+		endNodeID = Data.getEndNodeIDArray();
+		
+		amountOfNodes = edgeOffset.length;
+		amountOfEdges = endNodeID.length;
+		
+		priorityQueue = new int[amountOfNodes];
+		priorityQueue[0] = source;
+		priorityQueueLength = 1;
+		
+		int reachableNodes = 0;
+		boolean[] alreadyVisited = new boolean[amountOfNodes];
+		
+		alreadyVisited[0] = true;
+
+		while (true) {
+			int currNode = priorityQueue[0];
+			int currNodeEdgesFrom = edgeOffset[currNode];
+			int currNodeEdgesTo;
+			
+			if (currNodeEdgesFrom != -1) {
+				if (currNode + 1 == amountOfNodes)
+					currNodeEdgesTo = amountOfEdges;
+				else
+					currNodeEdgesTo = edgeOffset[currNode + 1];
+
+				for (int ctr = currNodeEdgesFrom; ctr < currNodeEdgesTo; ctr++) {
+
+					if (!alreadyVisited[endNodeID[ctr]]) {
+						reachableNodes++;
+						alreadyVisited[endNodeID[ctr]] = true;
+						priorityQueue[priorityQueueLength] = endNodeID[ctr];
+						priorityQueueLength++;
+					}
+				}
+			}
+			
+			if (priorityQueueLength == 0) {
+				System.out.println(reachableNodes + "/" + amountOfNodes + " edges are reachable from " + source);
+				System.out.println(Utility.endTimer());
+				
+				for (int i = 0; i < alreadyVisited.length; i++)
+					if (!alreadyVisited[i] && i == 200)
+						System.out.println(i);
+				return;
+			}
+			
+			priorityQueueLength--;
+			priorityQueue[0] = priorityQueue[priorityQueueLength];
+		}
+	}
+
 }
